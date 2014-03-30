939f6c75e0757ca684a30667d253d1657822fb95
DavidAmmeraal <david.ammeraal@gmail.com>
2014-03-30 22:58:32 +0200

    Initial commit

--- public/javascripts/vendor/facebook/fb.js
+++ public/javascripts/vendor/facebook/fb.js
@@ -1,6 +1,6 @@
 define(['facebook'], function(){
     FB.init({
-        appId      : '252866621564945'
+        appId: 252866621564945
     });
     FB.getLoginStatus(function(response) {
         console.log(response);
