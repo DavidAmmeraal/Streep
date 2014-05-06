44b26f4c6a0deb11264ce1cd7e3031807a1c3375
DavidAmmeraal <david.ammeraal@gmail.com>
2014-05-06 20:53:05 +0200

    Initial commit

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ var driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.chrome()).
     build();
 driver.get('chrome://gpu')
-driver.executeScript('window.scrollBy(0,850)');
+driver.executeScript('window.scrollBy(0,1150)');
 //END selenium
 
 
